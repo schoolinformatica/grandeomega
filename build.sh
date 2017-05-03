13d8f904da18927f3d2a11be8546dbe75aa8132c
Steven Schenk <stevenschenk74@hotmail.com>
2017-05-03 11:40:17 +0200

    Change dotnet version travis

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 sudo sh -c 'echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ trusty main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 417A0893
 sudo apt-get update
-sudo apt-get install dotnet-dev-1.0.1
+sudo apt-get install dotnet-dev-1.0.3
 
 repoFolder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $repoFolder
